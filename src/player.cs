a1e45aec4e5670124727024e2f8aafb82a4d2213
Kevin Zhang <kevin@Kevins-MacBook-Pro.local>
2024-03-13 12:53:14 -0400

    Parallex

--- src/player.cs
+++ src/player.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class player : CharacterBody2D
 {
-	public const float Speed = 400.0f;
-	public const float JumpVelocity = 400.0f;
+	public const float Speed = 1.5f * 400.0f;
+	public const float JumpVelocity = 10 * 400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -12,6 +12,7 @@ public partial class player : CharacterBody2D
 	// Get the animation child node
 	AnimatedSprite2D animation = null;
 
+	// Initializer for Godot node
 	public override void _Ready()
 	{
 		animation = GetNode<AnimatedSprite2D>("animation");
@@ -60,17 +61,17 @@ public partial class player : CharacterBody2D
 		
 		if (Math.Abs(Velocity.X) > Speed / 100)
 		{
-			animation.Animation = "running";
-			
+			animation.Play("running");
+
 		} else {
-			animation.Animation = "default";
+			animation.Play("default");
 		}
 
 		if (Velocity.Y < 0) {
-			animation.Animation = "jumping";
+			animation.Play("jumping");
 		}
 		else if (Velocity.Y > 0) {
-			animation.Animation = "falling";
+			animation.Play("falling");
 		}
 	}
 }
